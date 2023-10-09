35cb8759805c8ce46d7538b2c90b566b35958320
erbaghdasaryan <120387336+ErBaghdasaryan@users.noreply.github.com>
2023-10-10 03:26:06 +0400

    Updating view

--- WeatherAPI/Models/NetworkWeatherManager.swift
+++ WeatherAPI/Models/NetworkWeatherManager.swift
@@ -2,29 +2,39 @@
 
 import Foundation
 
-struct NetworkWeatherManager {
+protocol NetworkWeatherManagerDelegate: AnyObject {
+    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
+}
+
+class NetworkWeatherManager {
+    
+    weak var delegate: NetworkWeatherManagerDelegate?
     
     func fetchCurrentWeather(city: String) {
-        
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=ccee4c995a16a4cbd16a4ef36dcfb205"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=ccee4c995a16a4cbd16a4ef36dcfb205&units=metric"
         guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                self.parseJSON(data: data)
+                if let currentWeather = self.parseJSON(data: data) {
+                    self.delegate?.updateInterface(self, with: currentWeather)
+                }
             }
         }
         task.resume()
     }
     
-    func parseJSON(data: Data) {
+    func parseJSON(data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         do {
            let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
-            print(currentWeatherData.main.temp)
-
+            guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData) else {
+                return nil
+            }
+            return currentWeather
         } catch let error as NSError {
             print(error.localizedDescription)
         }
+        return nil
     }
 }
--- WeatherAPI/View Controllers/ViewController.swift
+++ WeatherAPI/View Controllers/ViewController.swift
@@ -9,18 +9,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     
-    let networkWeatherManager = NetworkWeatherManager()
-    
+    var networkWeatherManager = NetworkWeatherManager()
     
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [unowned self] city in
             self.networkWeatherManager.fetchCurrentWeather(city: city)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        networkWeatherManager.delegate = self
         networkWeatherManager.fetchCurrentWeather(city: "London")
+    }
+}
+
+//MARK: NetworkWeatherManagerDelegate
+extension ViewController: NetworkWeatherManagerDelegate {
+    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather) {
+        DispatchQueue.main.async{
+            self.cityLabel.text = currentWeather.cityName
+            self.temperatureLabel.text = currentWeather.temperatureString
+            self.feelsLikeTemperatureLabel.text = currentWeather.feelsLikeTemperatureString
+            self.weatherIconImageView.image = UIImage(systemName: currentWeather.systemIconNameString)
+        }
     }
 }
 
