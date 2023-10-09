0c7ad74239370754af865fbb72b22984de15ad8b
erbaghdasaryan <120387336+ErBaghdasaryan@users.noreply.github.com>
2023-10-10 02:24:40 +0400

    Parsing JSON

--- WeatherAPI/Models/NetworkWeatherManager.swift
+++ WeatherAPI/Models/NetworkWeatherManager.swift
@@ -1,6 +1,6 @@
 //  Created by Er Baghdasaryan on 09.10.23.
 
-import UIKit
+import Foundation
 
 struct NetworkWeatherManager {
     
@@ -11,10 +11,20 @@ struct NetworkWeatherManager {
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                let dataString = String(data: data, encoding: .utf8)
-                print(dataString!)
+                self.parseJSON(data: data)
             }
         }
         task.resume()
+    }
+    
+    func parseJSON(data: Data) {
+        let decoder = JSONDecoder()
+        do {
+           let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
+            print(currentWeatherData.main.temp)
+
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
 }
