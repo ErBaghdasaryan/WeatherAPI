77742560e95db47ed3342990e04fbcb9372a30c4
erbaghdasaryan <120387336+ErBaghdasaryan@users.noreply.github.com>
2023-10-10 04:03:21 +0400

    Final commit

--- WeatherAPI/Models/NetworkWeatherManager.swift
+++ WeatherAPI/Models/NetworkWeatherManager.swift
@@ -1,6 +1,7 @@
 //  Created by Er Baghdasaryan on 09.10.23.
 
 import Foundation
+import CoreLocation
 
 protocol NetworkWeatherManagerDelegate: AnyObject {
     func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
@@ -10,8 +11,27 @@ class NetworkWeatherManager {
     
     weak var delegate: NetworkWeatherManagerDelegate?
     
-    func fetchCurrentWeather(city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=ccee4c995a16a4cbd16a4ef36dcfb205&units=metric"
+    enum RequestType {
+        case cityName(String)
+        case coordinate(latitude: CLLocationDegrees,
+                        longitude: CLLocationDegrees)
+    }
+    
+    func fetchCurrentWeather(forRequestType requestType: RequestType) {
+        
+        var urlString = ""
+        
+        switch requestType {
+        case .cityName(let city):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=ccee4c995a16a4cbd16a4ef36dcfb205&units=metric"
+        case .coordinate(let latitude, let longitude):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=ccee4c995a16a4cbd16a4ef36dcfb205&units=metric"
+        }
+        
+        performRequest(withURLString: urlString)
+    }
+
+    func performRequest(withURLString urlString: String) {
         guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
--- WeatherAPI/View Controllers/ViewController.swift
+++ WeatherAPI/View Controllers/ViewController.swift
@@ -1,6 +1,7 @@
 //  Created by Er Baghdasaryan on 09.10.23.
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -10,10 +11,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     
     var networkWeatherManager = NetworkWeatherManager()
+    lazy var locationManager: CLLocationManager = {
+        let lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyKilometer
+        lm.requestWhenInUseAuthorization()
+        return lm
+    }()
     
     @IBAction func searchPressed(_ sender: UIButton) {
         self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [unowned self] city in
-            self.networkWeatherManager.fetchCurrentWeather(city: city)
+            self.networkWeatherManager.fetchCurrentWeather(forRequestType: .cityName(city))
         }
     }
     
@@ -21,7 +29,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         networkWeatherManager.delegate = self
-        networkWeatherManager.fetchCurrentWeather(city: "London")
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestLocation()
+        }
     }
 }
 
@@ -34,6 +45,22 @@ extension ViewController: NetworkWeatherManagerDelegate {
             self.feelsLikeTemperatureLabel.text = currentWeather.feelsLikeTemperatureString
             self.weatherIconImageView.image = UIImage(systemName: currentWeather.systemIconNameString)
         }
+    }
+}
+
+//MARK: CALocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let letitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        networkWeatherManager.fetchCurrentWeather(forRequestType: .coordinate(latitude: letitude, longitude: longitude))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
 
